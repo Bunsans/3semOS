c3854ab2072c04d4d83acfa6ca3bde5d1b68d4fe
Bunsans <sandr474@gmail.com>
2022-02-16 00:51:57 +0300

    add loop for buffer

--- 3/3_1.c
+++ 3/3_1.c
@@ -7,56 +7,83 @@
 #include <sys/stat.h>
 #include <sys/sysmacros.h>
 
-int main(int argc, char const *argv[]) {
+#define BUF_SIZE 1024 * 1024
+
+int main(int argc, char const *argv[])
+{
     int result = 0;
 
-    if (argc != 3) {
+    if (argc != 3)
+    {
         fprintf(stderr, "Usage: %s src dest\n", argv[0]);
         return 1;
     }
 
     struct stat sb;
-    if (lstat (argv[1], &sb) == -1) {
+    if (lstat(argv[1], &sb) == -1)
+    {
         perror("lstat failed");
         return 2;
     }
 
-    if ((sb.st_mode & S_IFMT) != S_IFREG) {
+    if ((sb.st_mode & S_IFMT) != S_IFREG)
+    {
         perror("Not a regular file");
         return 3;
     }
 
-    int fd1 = open (argv[1], O_RDONLY);
-    if (fd1 < 0) {
+    int fd1 = open(argv[1], O_RDONLY);
+    if (fd1 < 0)
+    {
         perror("Failed to open src file");
         return 4;
     }
-    //0644 - r/w for user, read-only for group and others
-    int fd2 = open (argv[2], O_RDWR | O_CREAT | O_TRUNC, 0644);
-    if (fd2 < 0) {
+    // 0644 - r/w for user, read-only for group and others
+    int fd2 = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0644);
+    if (fd2 < 0)
+    {
         perror("Failed to open or create dest file");
         close(fd1);
         return 5;
     }
 
-    char buf[sb.st_size];//файл может не влезть
+    char buf[BUF_SIZE];
+    ssize_t nbytes, nbytes_w;
 
-    if (read(fd1, &buf, sb.st_size) == -1) {
-        perror("Failed to read src file");
-        return 6;
+    off_t offset_r = 0, offset_w = 0;
+    while ((nbytes = read(fd1, &buf + offset_r, sb.st_size)) > 0) // считываем sb.st_size байтов из fd1 начиная с &buf + offset_r
+    {
+        offset_r += nbytes; // увеличиваем смещение
+        while (nbytes)
+        {
+            nbytes_w = write(fd2, &buf + offset_w, (long unsigned)nbytes); // записываем nbytes байтов в fd2 начиная с &buf + offset_w
+            if (nbytes_w == -1)
+            {
+                close(fd1);
+                close(fd2);
+                perror("pwrite failed");
+                return 6;
+            }
+            nbytes -= nbytes_w;   // уменьшаем количество байтов которые нужно записать
+            offset_w += nbytes_w; // увеличиваем смещение
+        }
     }
 
-    if (dprintf(fd2, "%s", buf) != sb.st_size) {
-        perror ("dprintf failed");
+    if (nbytes == -1) // обработка ошибки read
+    {
+        perror("read failed");
         result = 7;
     }
 
-    if (close(fd1) < 0) {
+    // закрываем дескрипторы
+    if (close(fd1) < 0)
+    {
         perror("Closing src file failed");
         result = 8;
     }
 
-    if (close(fd2) < 0) {
+    if (close(fd2) < 0)
+    {
         perror("Closing dest file failed");
         result = 9;
     }
