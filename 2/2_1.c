1261c171ef3f973273239c4621a898a811aa0ac3
Bunsans <sandr474@gmail.com>
2022-02-16 20:16:14 +0300

    add comments

--- 2/2_1.c
+++ 2/2_1.c
@@ -6,41 +6,49 @@
 #include <string.h>
 #include <unistd.h>
 
-ssize_t writeall(int fd, const void *buf, size_t count) {
+ssize_t writeall(int fd, const void *buf, size_t count)
+{
     size_t bytes_written = 0;
     const uint8_t *buf_addr = buf;
-    while (bytes_written < count) {
-        ssize_t res = write(fd, buf_addr + bytes_written, count - bytes_written);
-        if (res < 0) {
+    while (bytes_written < count)
+    {
+        ssize_t res = write(fd, buf_addr + bytes_written, count - bytes_written); // записываем count - bytes_written байт строки начиная с buf_addr + bytes_written
+        if (res < 0)                                                              
+        {
             return res;
         }
-        bytes_written += (size_t)res;
+        bytes_written += (size_t)res; // увеличиваем количество записанных байтов на сколько смог write записать, потом опять итерация цикла пока не запишем все что нужно
     }
     return (ssize_t)bytes_written;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
     int result = 0;
-    
-    if (argc != 3) {
+
+    if (argc != 3)
+    {
         fprintf(stderr, "Usage: %s path text\n", argv[0]);
         return 1;
     }
-    //0644 - r/w for user, read-only for group and others
-    int fd = open (argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    // 0644 - r/w for user, read-only for group and others
+    int fd = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0644); // открываем файл куда записываем
 
-    if (fd < 0) {
+    if (fd < 0)
+    {
         perror("Failed to open file for writing");
         return 2;
     }
 
-    if (writeall(fd, argv[2], strlen(argv[2])) < 0) {
-        perror ("Failed to write");
+    if (writeall(fd, argv[2], strlen(argv[2])) < 0) // записываем строку
+    {
+        perror("Failed to write");
         result = 3;
     }
 
-    if (close(fd) < 0) {
+    if (close(fd) < 0)
+    {
         perror("Failure during close");
         result = 4;
     }
--- 2/2_2.c
+++ 2/2_2.c
@@ -4,30 +4,35 @@
 #include <string.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
     int result = 0;
 
-    if (argc != 3) {
+    if (argc != 3)
+    {
         printf("Usage: %s filename string\n", argv[0]);
         return 1;
-    }   
-    //0644 - r/w for user, read-only for group and others
-    int fd = open(argv[1], O_RDWR|O_CREAT|O_APPEND, 0644);
-    if (fd < 0) {
+    }
+    // 0644 - r/w for user, read-only for group and others
+    int fd = open(argv[1], O_RDWR | O_CREAT | O_APPEND, 0644); // открываем файл куда будем записывать
+    if (fd < 0)
+    {
         perror("File can't be opened or created");
         return 2;
-    }   
+    }
 
-    if (dprintf(fd, "%s", argv[2]) != (int)strlen(argv[2])) {
+    if (dprintf(fd, "%s", argv[2]) != (int)strlen(argv[2]))
+    { // записываем строку
         perror("dprintf failed");
         result = 3;
     }
 
-    if (close(fd) == -1) {
+    if (close(fd) == -1)
+    {
         perror("Failed to close");
         result = 4;
     }
 
     return result;
-} 
+}
